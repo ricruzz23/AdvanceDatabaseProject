064fb6515930d39620760724820b300ab483806d
z003y54m <z003y54m@md1rs0rc.ad001.siemens.net>
2018-11-28 00:28:14 -0500

    Added the Add Employee fxml file and controller class. Added buttons to the home fxml file and set up methods.

--- atlantiscityapp/src/main/java/com/advancedb/atlantiscityapp/App.java
+++ atlantiscityapp/src/main/java/com/advancedb/atlantiscityapp/App.java
@@ -1,5 +1,13 @@
 package com.advancedb.atlantiscityapp;
 
+/**
+ * @author Roger Cruz
+ * 11/28/2018
+ * 
+ * This class is the Application's launch.
+ * 
+ * */
+
 import java.io.IOException;
 import java.util.Optional;
 import javafx.application.Application;
@@ -11,10 +19,6 @@ import javafx.scene.control.ButtonType;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-/**
- * Hello world!
- *
- */
 public class App extends Application
 {
     public static void main( String[] args )
@@ -40,6 +44,7 @@ public class App extends Application
 	
 	public void showHomeScene() throws IOException
 	{
+		
 		// Get the scene from the fxml file
 				FXMLLoader loader = new FXMLLoader(getClass().getResource("../mainview/home.fxml"));
 				mainLayout = loader.load();
@@ -48,24 +53,6 @@ public class App extends Application
 				primaryStage.setScene(scene);
 				primaryStage.show();
 	}
-	
-	public static void exitSystem() throws IOException
-	{
-		Alert alert = new Alert(AlertType.CONFIRMATION);
-		alert.setTitle("Exiting FloJo-Aide");
-		alert.setHeaderText(null);
-		alert.setContentText("Are you sure you want to exit?");
-
-		Optional<ButtonType> result = alert.showAndWait();
-		if (result.get() == ButtonType.OK)
-		{
-			System.exit(0);
-		}
-		else
-		{
-			alert.close();
-		}
-	}	
 	
 	// Private Data Members
 		private Stage primaryStage;
--- atlantiscityapp/src/main/java/com/advancedb/mainview/HomeController.java
+++ atlantiscityapp/src/main/java/com/advancedb/mainview/HomeController.java
@@ -1,15 +1,62 @@
 package com.advancedb.mainview;
 
-import java.io.IOException;
+/**
+ * @author Roger Cruz
+ * 11/28/2018
+ * 
+ * This class will control the Home Screen.
+ * 
+ * */
 
+import java.io.IOException;
+import java.util.Optional;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 public class HomeController {
 
 	@FXML protected void goToMainScene() throws IOException
-	{
-		
+	{	
+		try
+		{				
+			// Get the scene from the fxml file
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("../addEmp/AddEmployee.fxml"));
+			BorderPane updateJobPane = (BorderPane)loader.load();
+			Scene scene = new Scene(updateJobPane);
+			Stage stage = new Stage();
+			stage.setTitle("Update Job");
+			stage.setScene(scene);
+			stage.show();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
 	}
+	
+	@FXML protected void exitSystem() throws IOException
+	{
+		Alert alert = new Alert(AlertType.CONFIRMATION);
+		alert.setTitle("Exiting FloJo-Aide");
+		alert.setHeaderText(null);
+		alert.setContentText("Are you sure you want to exit?");
+
+		Optional<ButtonType> result = alert.showAndWait();
+		if (result.get() == ButtonType.OK)
+		{
+			System.exit(0);
+		}
+		else
+		{
+			alert.close();
+		}
+	}	
 	
 }
