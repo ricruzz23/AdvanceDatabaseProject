f6fafa70f9197243b18e94162bc80f91bb08220d
z003y54m <z003y54m@md1rs0rc.ad001.siemens.net>
2018-12-09 19:20:57 -0500

    trying to fix the pull

--- atlantiscityapp/src/main/java/com/advancedb/checkInCheckOutScene/checkInCheckOutController.java
+++ atlantiscityapp/src/main/java/com/advancedb/checkInCheckOutScene/checkInCheckOutController.java
@@ -4,13 +4,25 @@ import javafx.fxml.FXML;
 
 import javafx.scene.control.TextField;
 
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import com.advancedb.utilClasses.Customers;
+
 import javafx.event.ActionEvent;
 
 import javafx.scene.control.TableView;
 
 public class checkInCheckOutController {
 	@FXML
-	private TextField searchPhone;
+	private TextField searchCustID;
 	@FXML
 	private TextField custID;
 	@FXML
@@ -25,12 +37,29 @@ public class checkInCheckOutController {
 	private TableView tableViewAvailableToCheckout;
 	@FXML
 	private TableView tableViewCustomerHasCheckedout;
-
 	
+	Customers activeCustomer;
+
 	@FXML
 	public void searchCustomer(ActionEvent event)
 	{
+		Configuration con = new Configuration().configure().addAnnotatedClass(Customers.class);
+		SessionFactory sf = con.buildSessionFactory();
+		Session session = sf.openSession(); 
+		Transaction tx = session.beginTransaction();
+		Integer customerID = Integer.valueOf(searchCustID.getText());
 		
+		activeCustomer = session.get(Customers.class, customerID);
+		loadCustomerFromSearch(activeCustomer);
+	}
+	
+	public void loadCustomerFromSearch(Customers cust)
+	{
+		custID.setText(cust.getCustomers_customerID().toString());
+		firstName.setText(cust.getCustomer_firstName());
+		lastName.setText(cust.getCustomers_lastName());
+		phone.setText(cust.getCustomers_phoneNumber().toString());
+		email.setText(cust.getCustomers_email());
 	}
 	
 	@FXML
@@ -44,4 +73,5 @@ public class checkInCheckOutController {
 	{
 		
 	}
+	
 }
